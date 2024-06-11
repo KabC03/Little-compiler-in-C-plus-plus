8000bed1da5270c028199c8b8bf7df24f88de624
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-11 23:23:28 +1000

    Create compile.sh

---
+++ run/compile.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#Compile everything in ./src then run if compilation was ok
+
+
+files=""
+flags="-Wall -Werror -fsanitize=address -fno-omit-frame-pointer "
+
+
+for file in "src"/*.c; do
+
+    files+="$file "
+done
+
+
+mkdir -p output
+gcc $files $flags -o ./output/out
+prevOutput=$?
+
+
+
+if [[ $prevOutput -eq 0 ]]; then
+    clear
+    ./output/out
+
+else
+    echo "Compilation error"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
