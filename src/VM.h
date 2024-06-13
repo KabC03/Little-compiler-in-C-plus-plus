17ed6506edf6ba5a60330592f248355d85e463ac
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-14 05:12:25 +1000

    Update VM.h

--- src/VM.h
+++ src/VM.h
@@ -13,78 +13,9 @@ typedef struct VM VM;
 
 
 
-bool initialise_VM(size_t numberOfRegisters, size_t sizeOfRam);
+bool initialise_VM(size_t numberOfRegisters, size_t sizeOfRam, size_t stackSize);
 bool get_tokens_VM(char *IRfileName);
 bool run_VM(void);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
