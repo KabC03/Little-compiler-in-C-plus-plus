28558736bb3737fbc684a8744c0a42825e53184c
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-13 15:14:18 +1000

    Create VM.h

---
+++ src/VM.h
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+
+typedef struct VM VM;
+
+
+
+bool initialise_VM(size_t numberOfRegisters, size_t sizeOfRam);
+bool get_tokens_VM(char *IRfileName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
