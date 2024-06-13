f6fdc915fa4fd5907cb638f3b11f781ea7d16a91
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-13 22:06:06 +1000

    Update VM.h

--- src/VM.h
+++ src/VM.h
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <signal.h>
 
 
 
@@ -14,7 +15,7 @@ typedef struct VM VM;
 
 bool initialise_VM(size_t numberOfRegisters, size_t sizeOfRam);
 bool get_tokens_VM(char *IRfileName);
-
+bool run_VM(void);
 
 
 
