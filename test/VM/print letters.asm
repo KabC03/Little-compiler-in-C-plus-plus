abc085f20dae1ced5cf7db213cbc70f20f9e1729
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-17 06:24:59 +1000

    Update print letters.asm

--- test/VM/print letters.asm
+++ test/VM/print letters.asm
@@ -5,7 +5,7 @@
 
 
 [L] LABEL start
-    #char charVal = 'a'; for(int i = 0; i < 26; i++) {printf("%c", charVal++);};
+
 
     [I] ADD_I R3 R0 1
 
@@ -13,13 +13,13 @@
     [A] INPUT R2 R3 INTEGER
     [A] INPUT R4 R3 INTEGER
 
-
+    [I] ADD_I R4 R4 1
 [L] LABEL for_start
-    [C] BEQ_I R2 R4 end
+    [C] BEQ_I R2 R4 for_end
 
     [A] PRINT R2 R3 INTEGER
 
-    [I] ADD_I R2 R3 1
+    [I] ADD_I R2 R2 1
 
     [J] JUM for_start
 [L] LABEL for_end
