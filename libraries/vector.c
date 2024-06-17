b5e58e64e3b35a30cf8a008c2cca85d16f0dc70b
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-18 00:50:14 +1000

    Update vector.

--- libraries/vector.c
+++ libraries/vector.c
@@ -251,7 +251,8 @@ bool vector_resize(Vector *const vector, size_t offsetSize) {
         vector->size = (vector->size + offsetSize) * vector->elementSize;
         vector->data = realloc(vector->data, (vector->size + offsetSize ) * vector->elementSize);
 
-        if(vector->size < (vector->top) + 1) { //If the top is beyond the size some elements were lost
+
+        if((vector->size)/(vector->elementSize) < (vector->top) + 1) { //If the top is beyond the size some elements were lost
             vector->top = vector->size;
         }
 
@@ -300,3 +301,4 @@ bool vector_destroy(Vector *const vector) {
 
 
 
+ 
