b1186f98aa542d9a82796ea2bf7ec7810c47ee04
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-17 19:02:36 +1000

    Update vector.c

--- libraries/vector.c
+++ libraries/vector.c
@@ -1,1 +1,83 @@
+#include "vector.h"
+
+/**
+ * vector_get_size
+ * ===============================================
+ * Brief: Gets the size (in bytes) of a vector
+ * 
+ * Param: *vector - Pointer to the vector of interest
+ * Return: size_t - Size of a vector in bytes
+ * 
+ */
+size_t vector_get_size(Vector *vector) {
+
+
+    size_t size = 0;
+    if(vector == NULL) {
+        
+        return 0;
+
+    } else {
+
+        size = vector->size;
+
+    }
+    return size;
+}
+
+
+/**
+ * vector_get_len
+ * ===============================================
+ * Brief: Gets the len of a vector in bytes
+ * 
+ * Param: *vector - Pointer to the vector of interest
+ * Return: size_t - Size of a vector in bytes
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
