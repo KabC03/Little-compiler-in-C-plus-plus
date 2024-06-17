d84c99754758b244bf61c279163b1ee6d7c78da7
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-17 19:40:48 +1000

    Update vector.c

--- libraries/vector.c
+++ libraries/vector.c
@@ -1,5 +1,36 @@
 #include "vector.h"
 
+
+
+
+
+/**
+ * vector_get_size
+ * ===============================================
+ * Brief: Gets the size (in bytes) of a vector
+ * 
+ * Param: *vector - Pointer to the vector of interest
+ *        elementSize - sizeof(element) in bytes
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+bool vector_initialise(Vector *const vector, size_t elementSize) {
+
+    if(vector == NULL) {
+        return false;
+    } else {
+
+        vector->data = NULL;
+        vector->elementSize = 0;
+        vector->size = elementSize;
+        vector->top = 0;
+    }
+
+    return true;
+}
+
+
+
 /**
  * vector_get_size
  * ===============================================
@@ -9,7 +40,7 @@
  * Return: size_t - Size of a vector in bytes
  * 
  */
-size_t vector_get_size(Vector *vector) {
+size_t vector_get_size(const Vector *const vector) {
 
 
     size_t size = 0;
@@ -29,26 +60,92 @@ size_t vector_get_size(Vector *vector) {
 /**
  * vector_get_len
  * ===============================================
- * Brief: Gets the len of a vector in bytes
+ * Brief: Gets the length of the vector (zero based indexing)
  * 
  * Param: *vector - Pointer to the vector of interest
  * Return: size_t - Size of a vector in bytes
  * 
  */
+size_t vector_get_length(const Vector *const vector) {
+
+    size_t length = 0;
+    if(vector == NULL) {
+        return 0;
+    } else {
+        length = vector->top;
+    }
+
+    return length;
+}
+
+
+
+
+
+/**
+ * vector_get_index
+ * ===============================================
+ * Brief: Returns a pointer to an item in a vector (zero based indexing)
+ * 
+ * Param: *vector - Pointer to the vector of interest
+ *        index - Index of interest
+ * Return: uint8_t - Pointer to data within the vector
+ * 
+ */
+uint8_t* vector_get_index(Vector *const vector, size_t index) {
+
+    uint8_t *result = NULL;
+    if(vector == NULL) {
+        return NULL;
+    } else {
+
+        if(index > vector->top || vector->data == NULL) {
+            return NULL;
+        } else {
+
+            result = &((vector->data)[index * vector->elementSize]);
+        }
+    }
+
+    return result;
+}
+
+
+/**
+ * vector_insert_index
+ * ===============================================
+ * Brief: Insert an item at an index in the vector (zero based indexing)
+ * 
+ * Param: *vector - Pointer to the vector of interest
+ *        index - Index of interest
+ *        *data - Pointer to data to be inserted
+ * Return: bool - T/F depending on if insertion was successful
+ * 
+ */
+uint8_t* vector_insert_index(Vector *const vector, size_t index, void* data) {
+
+
+}
 
 
 
 
 
 
+/**
+ * vector_delete_index
+ * ===============================================
+ * Brief: Delete an item from the vector (zero based indexing)
+ * 
+ * Param: *vector - Pointer to the vector of interest
+ *        index - Index of interest
+ * Return: bool - T/F depending on if deletion was successful
+ * 
+ */
+uint8_t* vector_insert_index(Vector *const vector, size_t index) {
 
 
-
-
-
-
-
-
+}
 
 
 
