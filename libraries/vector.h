88e9c46ac7d2e36670c9ba490083ed6b7433309b
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-17 19:40:39 +1000

    Update vector.h

--- libraries/vector.h
+++ libraries/vector.h
@@ -7,24 +7,24 @@
 
 typedef struct Vector {
 
-    size_t size;         //Memory of vector
+    size_t size;         //Memory of vector (num of bytes)
 
-    size_t top;          //Top index of array
-    
+    size_t top;          //Top index of array (index)
+
     uint8_t* data;       //Data array pointer
     size_t elementSize;  //Size of individual element in array
 
 } Vector;
 
 
+bool vector_initialise(Vector *const vector, size_t elementSize);
+size_t vector_get_size(const Vector *const vector);
+size_t vector_get_length(const Vector *const vector);
 
 
-
-
-
-
-
-
+uint8_t* vector_get_index(Vector *const vector, size_t index);
+uint8_t* vector_insert_index(Vector *const vector, size_t index, void* data);
+uint8_t* vector_insert_index(Vector *const vector, size_t index);
 
 
 
