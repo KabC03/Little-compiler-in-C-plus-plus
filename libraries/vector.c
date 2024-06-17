c1b674be3886c06c5c5f3af48a5ffdf800f9fff4
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-17 21:08:33 +1000

    Update vector.c

--- libraries/vector.c
+++ libraries/vector.c
@@ -2,6 +2,8 @@
 
 
 
+
+
 /**
  * vector_get_size
  * ===============================================
@@ -122,6 +124,7 @@ uint8_t* vector_get_index(Vector *const vector, size_t index) {
  */
 bool vector_insert_index(Vector *const vector, size_t index, void* data) {
 
+
     if(vector == NULL || data == NULL) {
         return false;
     } else if(vector->data == NULL) {
@@ -130,12 +133,11 @@ bool vector_insert_index(Vector *const vector, size_t index, void* data) {
 
 
 
-        if(index > vector->top + 1) {
-
+        if(index > vector->top + 1 || vector->top + 1 > (vector->size)/(vector->elementSize)) { 
+            //top is the INDEX of the top element, size is the NUMBER OF BYTES in the array
             return false; //OOB error
             
         } else {
-
 
             for(int i = vector->top; i < index; i--) {
                 memcpy(&(vector->data)[(i * vector->elementSize) + 1], &(vector->data)[(i*vector->elementSize)], vector->elementSize);
@@ -168,11 +170,15 @@ bool vector_delete_index(Vector *const vector, size_t index) {
     if(vector == NULL) {
         return false;
 
+    } else if(vector->data == NULL) {
+        return false;
+
     } else {
 
 
 
-        if(index > vector->top + 1) {
+        if(index > vector->top + 1 || vector->top + 1 > (vector->size)/(vector->elementSize)) { 
+            //top is the INDEX of the top element, size is the NUMBER OF BYTES in the array
 
             return false; //OOB error
             
@@ -197,12 +203,25 @@ bool vector_delete_index(Vector *const vector, size_t index) {
  * Brief: Resize a vector by expanding or shrinking it (zero based indexing)
  * 
  * Param: *vector - Pointer to the vector of interest
- *        newSize - New vector sizeof(datatype) * number of elements
+ *        offsetSize - Offset of new size
  * Return: bool - T/F depending on if resize was successful
  * 
  */
-bool vector_resize(Vector *const vector, size_t newSize) {
+bool vector_resize(Vector *const vector, size_t offsetSize) {
 
+    if(vector == NULL) {
+        return false;
+    } else if(vector->size + offsetSize <= 0) {
+
+        return false;
+
+    } else {
+
+        vector->data = realloc(vector->data, (vector->size + offsetSize ) * vector->elementSize);
+        if(vector->data == NULL) {
+            return false;
+        }
+    }
     return true;
 }
 
