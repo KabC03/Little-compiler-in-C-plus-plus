a8c1ebd21d2cb6ef113b0cbb275bbb22b8786eff
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-17 19:02:45 +1000

    Update vector.h

--- libraries/vector.h
+++ libraries/vector.h
@@ -1,1 +1,92 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+
+
+typedef struct Vector {
+
+    size_t size;         //Memory of vector
+
+    size_t top;          //Top index of array
+    
+    uint8_t* data;       //Data array pointer
+    size_t elementSize;  //Size of individual element in array
+
+} Vector;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
