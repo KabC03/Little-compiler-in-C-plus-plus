588f55df9b808f3987b18567810ae3b18ad8ed0e
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-19 00:03:23 +1000

    Update hashmap.h

--- libraries/hashmap.h
+++ libraries/hashmap.h
@@ -1,1 +1,24 @@
+//Hashmap in C - allows collisions
+
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include "vector.h"
+
+typedef struct MapNode MapNode;
+
+typedef struct Map {
+    Vector mapNodes;        //Vector of mapnodes
+} Map;
+
+
+
+#endif // HASHMAP_H
+
+
 
