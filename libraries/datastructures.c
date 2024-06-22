53a1912b5ab2f3a7582a38ee5f7e26da3e98d026
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-23 06:49:47 +1000

    Update datastructures.c

--- libraries/datastructures.c
+++ libraries/datastructures.c
@@ -1,1 +1,182 @@
+#include "datastructures.h"
 
+
+struct Node {
+
+    uint8_t *data;
+    struct Node *next;
+};
+struct PriorityNode {
+
+    uint8_t *data;
+    struct PriorityNode *next;
+};
+
+
+//Stack
+
+/**
+ * stack_initialise
+ * ===============================================
+ * Brief: Initialise a stack before use
+ * 
+ * Param: *stack - stack of interest
+ *        dataSize - size of the datatype to be stored in the stack
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+bool stack_initialise(Stack *const stack, size_t dataSize) {
+
+    if(stack == NULL || dataSize == 0) {
+        return false;
+    } else {
+
+        stack->head = NULL;
+        stack->datatypeSize = dataSize;
+
+    }
+    return true;
+}
+
+
+
+
+/**
+ * stack_push
+ * ===============================================
+ * Brief: Push an item onto the stack
+ * 
+ * Param: *stack - stack of interest
+ *        *data - data to be pushed onto the stack
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+bool stack_push(Stack *const stack, void *data) {
+
+    if(stack == NULL || data == NULL) {
+        return false;
+    } else {
+
+        Node *newNode = malloc(sizeof(Node));
+        if(newNode == NULL) {
+            return false;
+        }
+        newNode->data = malloc(sizeof(uint8_t) * stack->datatypeSize);
+        if(newNode->data == NULL) {
+            free(newNode);
+            return false;
+        }
+        memcpy(newNode->data, data, stack->datatypeSize);
+
+        newNode->next = stack->head;
+        stack->head = newNode;
+    }
+
+    return true;
+}
+
+
+
+/**
+ * stack_pop
+ * ===============================================
+ * Brief: Pop an item onto the stack
+ * 
+ * Param: *stack - stack of interest
+ *        *result - output pointer of where the result should be stored
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+bool stack_pop(Stack *const stack, void *result) {
+
+    if(stack == NULL || result == NULL) {
+        return false;
+    } else {
+
+        memcpy(result, stack->head, stack->datatypeSize);
+
+        Node *freePtr = stack->head;
+
+        stack->head = stack->head->next;
+        free(freePtr);
+    }
+
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
