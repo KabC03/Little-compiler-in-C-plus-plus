c08785d3221cfb23bad4163f8c4e9bd1e4851c1a
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-26 03:24:27 +1000

    Update hashmap.h

--- libraries/hashmap.h
+++ libraries/hashmap.h
@@ -9,7 +9,7 @@
 #include <string.h>
 #include <stdint.h>
 #include "vector.h"
-
+#include "datastructures.h"
 
 
 typedef struct HashMap {
@@ -20,6 +20,7 @@ typedef struct HashMap {
 
 
 bool hashmap_hash_djb2(const uint8_t *const data, size_t dataSize, size_t tableSize, size_t *const hashOut); 
+bool hashmap_initialise(HashMap *const hashmap, size_t keySize, size_t valueSize, size_t initialTableSize);
 
 #endif // HASHMAP_H
 
