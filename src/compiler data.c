2d1ee7ec6c6aec84061c1aab8c7f3cc41045e37c
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-29 23:08:04 +1000

    Create compiler data.c

---
+++ src/compiler data.c
@@ -0,0 +1,62 @@
+#include "compile.h"
+
+
+
+//Put these into a hashmap in main
+const char KeyWords[][MAX_KEYWORD_SIZE] = {
+
+    "INT", "FLOAT", "CHAR",
+
+    "$DATA$", "$PROGRAM$", "$FUNCTION$"
+
+    "if", "elif", "else",
+    "while", "for",
+
+    "return",
+
+    "allocate", "free",
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
