f6eeeb9bab2f176f5d69753398f2c97d32312ecd
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-29 22:16:59 +1000

    Update hashmap.h

--- libraries/hashmap.h
+++ libraries/hashmap.h
@@ -13,15 +13,21 @@
 
 
 typedef struct HashMap {
-
+ 
     Vector mapListNodes;
+    size_t keySize;
+    size_t valueSize;
 
 } HashMap;
 
 
+
 bool hashmap_hash_djb2(const uint8_t *const data, size_t dataSize, size_t tableSize, size_t *const hashOut); 
-bool hashmap_initialise(HashMap *const hashmap, size_t keySize, size_t valueSize, size_t initialTableSize);
+bool hashmap_print(HashMap *const hashmap);
+bool hashmap_initialise(HashMap *const hashmap, size_t keySize, size_t valueSize, size_t TableSize);
+bool hashmap_insert(HashMap *const hashmap, void *const key, void *const value);
+bool hashmap_get_value(HashMap *const hashmap, void *const key, const void **valueOut);
+bool hashmap_remove(HashMap *const hashmap, void *const key);
+bool hashmap_destroy(HashMap *const hashmap);
 
 #endif // HASHMAP_H
-
-
