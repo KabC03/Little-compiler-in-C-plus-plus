db8fbd6e55ffa58eba929d5d1f2d6df8719411cb
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-29 23:17:41 +1000

    Create tokenise.h

---
+++ src/tokenise.h
@@ -0,0 +1,18 @@
+
+// Compiler data
+
+#ifndef TOKENISE_H
+#define TOKENISE_H 
+
+#include "compiler data.h"
+#include "datastructures.h"
+#include "hashmap.h"
+#include "vector.h"
+
+
+
+
+
+#endif // TOKENISE_H 
+
+
