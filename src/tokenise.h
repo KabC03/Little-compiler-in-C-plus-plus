3867d1847b7d31e19a1684f2e64a13311ea5470e
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-01 01:53:15 +1000

    Update tokenise.h

--- src/tokenise.h
+++ src/tokenise.h
@@ -3,15 +3,16 @@
 
 #ifndef TOKENISE_H
 #define TOKENISE_H 
-
+#include <ctype.h> 
+#include <stdlib.h>
 #include "compiler data.h"
 #include "datastructures.h"
 #include "hashmap.h"
 #include "vector.h"
 
 
-bool initialise_valid_token_hashmap(void);
-bool tokenise(char *line, Vector tokensOut);
+bool initialise_compiler_hashmaps (void);
+bool tokenise(char *line, Vector *const tokensOut);
 
 
 
