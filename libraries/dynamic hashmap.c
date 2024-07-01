73953dbb099ee54a68e619ba6b1f740d2f3d1a48
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-02 02:10:34 +1000

    Create dynamic hashmap.c

---
+++ libraries/dynamic hashmap.c
@@ -0,0 +1,66 @@
+#include "dynamic hashmap.h"
+
+
+
+//Initialise hashmap, set hashmap, destroy hashmap
+
+
+
+
+bool string_hashmap_initialise(DynamicHashmap *stringHashmap) {
+
+    if(stringHashmap == false) {
+        return false;
+
+
+    } else {
+
+
+
+    }
+    
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
