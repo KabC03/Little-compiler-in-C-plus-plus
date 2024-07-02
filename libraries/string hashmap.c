8c82d7cf06a755c11c7ebb73110599caaed1dcb4
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-03 04:54:25 +1000

    Update and rename dynamic hashmap.c to string hashmap.c

---
+++ libraries/string hashmap.c
@@ -0,0 +1,65 @@
+#include "string hashmap.h"
+
+
+
+
+
+/**
+ * string_hashmap_initialise 
+ * ===============================================
+ * Brief: Initialise a dynamic hashmap 
+ * 
+ * Param: *stringhashmap - Hashmap to initialise
+ *        initialhashmapSize - Initial size of the hashmap
+ * 
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+bool string_hashmap_initialise(StringHashmap *stringhashmap, size_t initialHashmapSize) {
+    
+    
+    
+    
+    
+    
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
