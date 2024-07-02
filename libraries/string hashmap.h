63292eed2293477a8ebe76d45ecc9a341dce5381
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-03 04:57:03 +1000

    Update string hashmap.h

--- libraries/string hashmap.h
+++ libraries/string hashmap.h
@@ -1,8 +1,8 @@
 
 //Hashmap for strings (since the other hashmap library doesnt work with them)
 
-#ifndef DYNAMIC_HASHMAP_H 
-#define DYNAMIC_HASHMAP_H 
+#ifndef STRING_HASHMAP_H
+#define STRING_HASHMAP_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -12,31 +12,18 @@
 #include "vector.h"
 #include "datastructures.h"
 
-typedef struct StringHashmapNode {
-
-    size_t keyLength;          //Key string size
-    size_t valueLength;        //Value string size
-    MapListNode mapListNode;   //Map nodes
-
-} StringHashmapNode;
-
 
 typedef struct StringHashmap{
 
-    Vector stringHashmapNode;  //Vector of string hashmap nodes 
+    Vector stringMapListNode;
 
 } StringHashmap;
-
-
-
-
-
 
 bool string_hashmap_initialise(StringHashmap *stringHashmap, size_t initialHashmapSize);
 
 
 
-#endif // DYNAMIC_HASHMAP_H 
+#endif // STRING_HASHMAP_H
 
 
 
