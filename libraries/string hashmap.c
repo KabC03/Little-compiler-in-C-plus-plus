0023cc59ed6ce727b49461edaa6186d1010ad737
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-04 04:41:12 +1000

    Update string hashmap.c

--- libraries/string hashmap.c
+++ libraries/string hashmap.c
@@ -1,7 +1,39 @@
 #include "string hashmap.h"
 
+#define STRING_DJB2_CONSTANT 5381
+#define string_hashmap_hash_djb2 string_hashmap_hash //Do this to select the current hash algorithm
 
+/**
+ * string_hashmap_hash_djb2
+ * ===============================================
+ * Brief: Perform a djb2 hash
+ * 
+ * Param: *data - Data to hash
+ *        dataSize - Size of data to hash
+ *        tableSize - Size of the table
+ *        *hashOut - Hash output
+ * 
+ * Return: bool - T/F depending on if freeing was successful
+ * 
+ */
+bool string_hashmap_hash_djb2(const uint8_t *const data, size_t dataSize, size_t tableSize, size_t *const hashOut) {
 
+    size_t hash= STRING_DJB2_CONSTANT;
+    if(data == NULL || dataSize == 0 || hashOut == NULL) {
+        return false;
+    } else {
+
+        for(size_t i = 0; i < dataSize; i++) {
+            
+            hash = (hash << 5) + hash + data[i];
+
+        }    
+        hash %= tableSize;
+        //printf("Recieved: %d || Hash: %zu\n", *(int*)data, hash);
+        *hashOut = hash;
+    }
+    return true;
+}
 
 
 /**
@@ -58,6 +90,41 @@ bool string_hashmap_initialise(StringHashmap *stringhashmap, size_t initialHashm
 
 
 
+
+/**
+ * string_hashmap_set 
+ * ===============================================
+ * Brief: Set a value in a hashmap given a key
+ * 
+ * Param: *stringhashmap - Hashmap to initialise
+ *        *key - Key to add to the hashmap
+ *        keySize - Size of the added key
+ *        *value - Value to add to the hashmap
+ *        valueSize - Size of the added value to the hashmap
+ * 
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+bool string_hashmap_set(StringHashmap *stringHashmap, void *key, size_t keySize, void *value, size_t valueSize) {
+    if(stringHashmap == NULL || key == NULL || value == NULL || keySize == 0 || valueSize == 0) {
+        
+        return false;
+    } else {
+
+        size_t hashIndex = -1;
+        
+        if(string_hashmap_hash(key, keySize, vector_get_size(&(stringHashmap->stringMapListNode)), &hashIndex) == false) {
+            return false;
+        }
+
+        if(string_map_LL_set((StringMapList*)(vector_get_index(&(stringHashmap->stringMapListNode), hashIndex)),
+        key, value, keySize, valueSize) == false) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 
 
