63bfe9bd4bc493053c6b674795682de3d94ddcaa
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-04 06:07:33 +1000

    Update string hashmap.h

--- libraries/string hashmap.h
+++ libraries/string hashmap.h
@@ -19,6 +19,8 @@ typedef struct StringHashmap{
 
 } StringHashmap;
 
+
+bool string_hashmap_print(StringHashmap *stringHashmap);
 bool string_hashmap_initialise(StringHashmap *stringHashmap, size_t initialHashmapSize);
 bool string_hashmap_set(StringHashmap *stringHashmap, void *key, size_t keySize, void *value, size_t valueSize);
 const void *string_hashmap_get_value(StringHashmap *stringHashmap, void *key, size_t keySize);
