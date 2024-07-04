fdce0bb62171dde84f74ee0f254066095f8418b6
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-04 22:38:09 +1000

    Update parse.c

--- src/parse.c
+++ src/parse.c
@@ -4,7 +4,35 @@
 
 
 
+/**
+ * parse 
+ * ===============================================
+ * Brief: Parse an array of tokens - Generate IR 
+ * 
+ * Param: *tokens - Vector of input tokens 
+ * 
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+bool parse(Vector *tokens) {
 
+    if(tokens == NULL) {
+        return false;
+
+    } else {
+
+        //If first token is a brace then pop off jump label from conditional statement
+        //Then if the next is a keyword then tokenise that as per normal
+        if(vector_get_length(tokens) == 0) {
+            printf("RECIEVED EMPTY TOKENS\n");
+            return false;
+        }
+
+
+    }
+
+    return true;
+}
 
 
 
