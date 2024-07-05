6eeff5fa786b91273c3314caf6c8375e88828261
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-05 23:39:58 +1000

    Update parse.c

--- src/parse.c
+++ src/parse.c
@@ -1,4 +1,4 @@
- #include "parse.h"
+#include "parse.h"
 
 
 
@@ -11,36 +11,58 @@ size_t globalVariableID;            //Used to keep track of the last label ID (t
 //Metadata for variables - held within the current stack frame
 typedef struct VariableMetadata {
 
-    TOKEN_TYPE dataType;             //int, float, chat, etc
-    size_t indirectionLvel;          //Level of indirection (number of '@')
-    size_t stackOffset;              //Offset from current base pointer
+    token_type datatype;                  //int, float, chat, etc
+    size_t indirectionlvel;               //level of indirection (number of '@')
+    size_t stackoffset;                   //offset from current base pointer
 
-} VariableMetadata;
+} variablemetadata;
 
 //Metadata for if, elif, else, for, while, etc statements
 typedef struct JumpMetadata {
 
-    size_t branchID;                 //e.g beq R1 R2 branchID (label ID)
-    size_t returnID;                 //At the end of an elif block when wanting to skip over next elif (label ID)
+    size_t branchID;                       //e.g beq R1 R2 branchID (label ID)
+    size_t returnID;                       //At the end of an elif block when wanting to skip over next elif (label ID)
+                                    
+    bool expectingOpenBrace;               //Used to denote that a '{' is expected (end of if statement) on the next line as the first token
+    bool seenIf;                           //Used to make sure elif or else is not declared before an if
+
+    //Statement should consume nex } token it sees
 
 } JumpMetadata;
 
 //Metadata for the current function (pushed onto an internal stack for functions)
 typedef struct FunctionMetadata {
 
-    size_t currentFrameSize;         //Frame size of LOCAL variables
-    StringHashmap localVariables;    //Hashmap of local variables (key = variabl name, value = VariableMetadata)
-    Stack JumpMetadata;              //Metadata for jumps (holds address for successful and non-successful jumps)
+    size_t currentFrameSize;              //Frame size of LOCAL variables
+    StringHashmap localVariables;         //Hashmap of local variables (key = variabl name, value = VariableMetadata)
+    Stack JumpMetadata;                   //Metadata for jumps (holds address for successful and non-successful jumps)
 
 } FunctionMetadata;
 
 
 
-Stack functionMetadata;              //Stack of function metadata
+typedef struct ProgramFlowMetadata {
+
+    bool mainIsDefined;                   //Main function has been seen
+    bool insideFunction;                  //Used to make sure a function is not declared inside another function
+
+} ProgramFlowMetadata;
+
+//Use these two directly
+Stack functionMetadata;                   //Stack of function metadata
+ProgramFlowMetadata programFlowMetadata;  //Metadata for program progression
+
+/*
+
+- If first call, write 'jum main' (unconditional jump to main function)
+- Next expecting a function declaration (no global variables)
+    - Main function takes no arguments but always returns an integer
 
 
+- By end of the program at least one main function should have been seen
 
 
+*/
 
 
 
