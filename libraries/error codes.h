e877c587e418627c0f249561000feb7b15dae9b1
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-09 02:23:40 +1000

    Create error codes.h

---
+++ libraries/error codes.h
@@ -0,0 +1,25 @@
+//Error codes for other libraries
+
+#ifndef ERROR_CODES_H
+#define ERROR_CODES_H
+
+typedef enum ERROR_CODES {
+
+    _NULL_PTR_PASS_,
+    _MEMORY_ALLOCATION_FAILURE_,
+
+    _GENERIC_FAILURE_,
+    _NEUTRAL_,
+    _SUCCESS_,
+
+} ERROR_CODES;
+
+
+
+#endif // ERROR_CODES_H 
+
+
+
+
+
+
