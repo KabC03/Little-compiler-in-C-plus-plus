8fe2f0af3960e24fc1fd0b594664121a1543e01a
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-10 05:23:46 +1000

    Update and rename error codes.h to return codes.h

---
+++ libraries/return codes.h
@@ -0,0 +1,30 @@
+//Return codes for other libraries
+
+#ifndef RETURN_CODE_H
+#define RETURN_CODE_H
+
+typedef enum RETURN_CODE {
+
+    _NULL_PTR_PASS_,
+
+    _MEMORY_ALLOCATION_FAILURE_,
+
+    _FAILED_TO_OPEN_FILE,
+    _FAILED_TO_CLOSE_FILE,
+
+
+    _GENERIC_FAILURE_,
+    _NEUTRAL_,
+    _SUCCESS_,
+
+} RETURN_CODE;
+
+
+
+#endif //RETURN_CODE_H 
+
+
+
+
+
+
