1cc990a0ab4c86d8bb50d09e0f62b1a976bfc356
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-10 21:28:58 +1000

    Update return codes.h

--- libraries/return codes.h
+++ libraries/return codes.h
@@ -13,6 +13,10 @@ typedef enum RETURN_CODE {
     _FAILED_TO_OPEN_FILE_,
     _FAILED_TO_CLOSE_FILE_,
 
+    _TRUE_,
+    _FALSE_,
+
+
     _GENERIC_FAILURE_,
     _NEUTRAL_,
     _SUCCESS_,
@@ -22,7 +26,6 @@ typedef enum RETURN_CODE {
 
 
 #endif //RETURN_CODE_H 
-
 
 
 
