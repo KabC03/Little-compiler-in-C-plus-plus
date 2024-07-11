5cb3bfcef23658e2e817723f0969bfebb26a8778
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-11 19:08:56 +1000

    Update parse.h

--- src/parse.h
+++ src/parse.h
@@ -1,6 +1,7 @@
 
+//Parser - generates IR
 
-#ifndef PARSE_H
+#ifndef PARSE_H 
 #define PARSE_H
 
 #include <stdlib.h>
@@ -8,21 +9,10 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdint.h>
-#include <math.h>
-#include "vector.h"
-#include "datastructures.h"
-#include "dynamic string.h"
-#include "static hashmap.h"
-#include "string hashmap.h"
-#include "compiler data.h"
-#include "tokenise.h"
-#include "IR grammer.h"
-#include "error codes.h"
-#define INITIAL_MAP_SIZE 100
 
 
-bool parser_initialise_structures(FILE *irFile);
-bool parse(Vector *tokens);
 
 
-#endif // PARSE_H
+
+
+#endif //PARSE_H
