a9241b953fb371a6a99bd863e5f1d8c7c2f9a32a
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-13 23:34:51 +1000

    Update set compiler data.py

--- src/set compiler data.py
+++ src/set compiler data.py
@@ -1,239 +1,250 @@
-# 10, Jul, 24
-# This file generates the compiler data.h and compiler data.c files
-import sys
+#Generates the data section for the asm program - also write the hash table in assembly
+
+import sys;
+
+dataFileName = "data.asm";
+djb2HashSeed = 5381; #Will get changed by perfect hash function generator
+maxTableTries = 10; #Max tries to find a perfect constant before giving up
+perfectHashTable = []; #Perfect hash table array
+
+#Number of bytes required for inidividual items - update manually
+pneumonicLength = 3;
+machineCodeLength = 1;
 
 
-#Print a newline every newLineConstant lines in the .c and .h files
-newLineConstant = 5;
 
+sourceCodePneumonics = {
 
-defineNameNumberOfTokens = "NUMBER_OF_TOKENS";
-defineNameMaxTokenLength = "MAX_TOKEN_LENGTH";
-tokenStruct = """
+    #Pneumonic : Machine code
 
-typedef struct Token {
+    #Instructions
+    "ADD" : "0", "LIR" : "1", "PSH" : "2", "POP" : "3", "JEQ" : "4", "BLK" : "-1",
 
-    VALID_TOKEN_ENUM tokenEnum;
+    #Registers
+    "EAX" : "0", "EBX" : "1", "ECX" : "2", "EDX" : "3",
 
-    //Immediates and variables
-    union {
-        DynamicString userString;  //User string (func name, variable, etc - parser must determine based on context)
+};
+hashTableSize = len(sourceCodePneumonics.keys());
+preAppendDefines = {
 
-        char charImmediate;        //Used if immediate char 'f'
-        int intImmediate;          //Used for immediate int '3'
-        float floatImmediate;      //Used for immediate float '3.32'
-    };
+    #File processing
+    "LINE_BUFFER_SIZE" : 1000, 
 
-} Token;
-""";
-
-
-hFile = "./compiler data.h";
-cFile = "./compiler data.c";
-headerGuardName = "COMPILE_H";
-
-
-#Tokens to pre-append (Appear in .h but not .c file)
-tokensPreappend = [
-
-    "EOF_TOKEN",
-    "INT_IMMEDIATE",
-    "FLOAT_IMMEDIATE",
-    "CHAR_IMMEDIATE",
-    "USER_STRING",
-
-];
-
-
-#Tokens to append to the files
-tokens = {
-
-    #Internal types
-    "invalid" : "INVALID_TOKEN",
-
-    #Datatypes
-    "int" : "TOK_INT", "flt" : "TOK_FLT", "chr" : "TOK_CHR", "@" : "TOK_PTR", "=" : "TOK_EQUALS_ASSIGNMENT",
-
-    #Operators
-    "+" : "TOK_ADD", "-" : "TOK_SUB", "*" : "TOK_MUL", "/" : "TOK_DIV","\%" : "TOK_MOD","||" : "TOK_OR", "&&" : "TOK_AND",
-    "==" : "TOK_EQUAL_TO", "!=" : "TOK_NOT_EQUAL_TO", ">=" : "TOK_GREATER_EQUAL_TO", "<=" : "TOK_LESS_EQUAL_TO", ">>" : "TOK_GREATER_TO", "<<" : "TOK_LESS_TO", 
-
-
-    #Control flow
-    "if" : "TOK_IF", "gto" : "TOK_GTO", "lbl" : "TOK_LBL",
-
-    #Functions
-    "fn" : "TOK_FN", "ret" : "TOK_RET",
-
-    #Inbuilt functions
-    "allocate" : "TOK_ALLOCATE", "free" : "TOK_FREE", "sizeof" : "TOK_SIZEOF",
-
-    #Organisation
-    "//" : "TOK_COMMENT",
-
-    #Misc
-    "<" : "TOK_OPEN_ANGLE", ">" : "TOK_CLOSE_ANGLE", "[" : "TOK_OPEN_SQUAER", "]" : "TOK_CLOSE_SQUARE", 
-    "(" : "TOK_OPEN_PAREN", ")" : "TOK_CLOSE_PAREN", "{" : "TOK_OPEN_CURLEY", "}" : "TOK_CLOSE_CURLEY", 
-    "," : "TOK_COMMA", "." : "TOK_DOT", ";" : "TOK_SEMICOLEN", "\'" :"TOK_SINGLE_QUOTE",
 };
 
-numberOfTokens = 0;
-maxTokenSize = 0;
 
 
 
-
-librariesToInclude = [
-
-    "<stdlib.h>",
-    "<stdio.h>",
-    "<stdbool.h>",
-    "<string.h>",
-    "<stdint.h>",
-    "\"vector.h\"",
-    "\"datastructures.h\"",
-    "\"static hashmap.h\"",
-    "\"dynamic string.h\"",
-];
+#Hash a key
+def djb2_hash(key):
 
 
 
-#Write to the .h file
-def write_h_file():
+    djb2Hash = djb2HashSeed;
+    for character in key:
+        djb2Hash = ((djb2Hash * 33) + djb2Hash) + ord(character);
 
-    try:
-        
-        with open(hFile, 'w') as file:
-        
-            #Write header guards
-            file.write("#ifndef " + str(headerGuardName) + "\n");
-            file.write("#define " + str(headerGuardName) + "\n\n\n");
+    #print(str(djb2Hash % 10) + "   "  + str(key));
 
-            #Define constants
-            file.write("#define " + str(defineNameNumberOfTokens) + " " + str(numberOfTokens) + "\n");
-            file.write("#define " + str(defineNameMaxTokenLength) + " " + str(maxTokenSize) + "\n\n\n");
+    return djb2Hash % hashTableSize;
 
 
-            #Include libraries
-            for library in librariesToInclude:
-                file.write("#include " + str(library) + "\n");
+#Convert a string into ASCII
+def convert_string_to_ASCII(string):
 
-            file.write("extern const char validTokens[" + str(defineNameNumberOfTokens) + "][" + str(defineNameMaxTokenLength) + "];\n" ) 
-
-            file.write("\n\ntypedef enum VALID_TOKEN_ENUM {\n\n");
-
-
-            counter = -1;
-            for token in tokensPreappend:
-                
-                if (-1 * counter) % newLineConstant == 0:
-                    file.write("\n\n");
-                file.write("    " + str(token) + " = " + str(counter) + ",\n");
-                counter -= 1;
-
-            counter = 0;
-            for value in tokens.values():
-
-                if counter % newLineConstant == 0:
-                    file.write("\n\n");
-
-                file.write("    " + str(value) + " = " + str(counter) + ",\n");
-                counter += 1;
-
-            file.write("\n\n} VALID_TOKEN_ENUM;\n");
+    asciiString = "";
+    for character in string:
+        asciiString += str(ord(character));
+    return asciiString;
 
 
-            file.write("\n\n" + str(tokenStruct) + "\n\n");
+#Find a perfect hash function
+def find_perfect_djb2_hash_constant():
+    global djb2HashSeed;
+    global perfectHashTable;
+
+    perfectHashTable = list(sourceCodePneumonics.keys());
+    tableIsPerfect = False;
+    tries = 0;
+    while(tableIsPerfect == False):
+
+        #Exit if max tries are reached
+        if(tries == maxTableTries):
+            print("Could not find perfect hash within '" + str(maxTableTries) + "' tries\n");
+            return False;
 
 
 
-
-            file.write("\n\n#endif //" + str(headerGuardName) + "\n\n");
-            file.write("//NOTE: Generated by python script\n\n\n\n");
-
-
+        #Zero out the table
+        for i in range(0,len(perfectHashTable)):
+            perfectHashTable[i] = None;
 
 
+        #Hash all keys in the table - if a collision is detected try again with a different hash constant
+        tableIsPerfect = True; #Assume table is perfect until collision met
+        for key in sourceCodePneumonics:
 
+            indexToInsert = djb2_hash(key);
 
-        return True; 
-    
-    
-    except:
-        return False;
-
-
-
-
-#Write to the .c file
-def write_c_file():
-
-    global numberOfTokens;
-    global maxTokenSize;
-
-    numberOfTokens = len(tokens);
-    maxTokenSize = 0;
-    for token in tokens:
-        if len(token) > maxTokenSize:
-            maxTokenSize = len(token);
-
-    maxTokenSize += 1; #Make space for NULL terminator
-
-    try:
-        with open(cFile, 'w') as file:
-            file.write("#include " + "\"" + str(hFile) + "\"\n\n\n");
-
-
-            file.write("const char validTokens[" + str(defineNameNumberOfTokens) + "][" + str(defineNameMaxTokenLength) + "] = {\n");
-
-            counter = 0;
-            for key in tokens.keys():
-
-                if counter % newLineConstant == 0:
-                    file.write("\n\n\n\n    ");
-
-                file.write("\"" + key + "\",");
+            if(perfectHashTable[indexToInsert] != None):
+                #Collision detected
+                #print("Collision with: " + str(hashTable[indexToInsert]));
+                tableIsPerfect = False;
+                break;
             
-                counter += 1;
+            else:
+                #Insert at the index - mark it as used
+                perfectHashTable[indexToInsert] = str(indexToInsert);
 
-            file.write("\n\n};\n\n");
-        
-            file.write("//NOTE: Generated by python script\n\n\n\n");
-        return True; 
-    
-    
+        djb2HashSeed += 1;
+        tries += 1; 
+
+    return True;
+
+
+
+
+def write_defines():
+    try:
+        with open(dataFileName, 'w') as file:
+
+            file.write("; Defines section\n\n");
+
+            for key, value in preAppendDefines.items():
+                file.write("%define " + str(key) + " " + str(value) + "\n");
+
+
+            file.write("\n\n\n");
+
+        return True;
     except:
         return False;
 
 
 
-#Main function
+
+#Write .data section
+def write_data_section() :
+    try:
+        with open(dataFileName, 'a') as file:
+
+            file.write("; .data section\n");
+            file.write("section .data\n");
+
+
+            file.write("\n\n\n");
+        return True;
+    except:
+        return False;
+
+
+#Write .bss section
+def write_bss_section():
+
+
+    try:
+        with open(dataFileName, 'a') as file:
+
+            file.write("; .bss section\n");
+            file.write("section .bss\n");
+
+            #Space for key hashmap
+            #file.write("    keyBuffer: db " + str(len(sourceCodePneumonocs.keys()) * pneumonicLength) + "\n"); #Keys dont need to be stored
+            file.write("    valueBuffer: db " + str(len(sourceCodePneumonics.values()) * machineCodeLength) + "\n");
+
+
+
+
+        return True;
+    except:
+        return False;
+
+
+
+def write_text_section():
+
+
+    try:
+        with open(dataFileName, 'a') as file:
+
+            file.write("; .text section\n");
+            file.write("section .section\n\n");
+
+            #Store the perfect hash table in the .bss block
+            #Just iterate over each character and store it as a byte
+            #Store 1, 2 or 4 bytes depending on the remaining lenght of the string
+
+            file.write("    push eax\n");
+            file.write("    lea edi, [valueBuffer]\n");
+            index = 0;
+            remainingLength = len(sourceCodePneumonics.values());
+
+            perfectHashTable = sourceCodePneumonics.keys();
+            stringToWrite = "".join(perfectHashTable);
+
+            while(remainingLength != 0):
+                immediate = 0;
+
+                if(remainingLength - 4 >= 0):
+                    #Write the next 4 characters
+                    
+                    immediate = convert_string_to_ASCII(stringToWrite[index:index+4]);
+                    file.write("    mov dword [edi], " + str(immediate) + "\n");
+                    remainingLength -= 4;
+                    index += 4;
+
+                elif(remainingLength - 2 >= 0):
+                    #Write the next 2 characters
+                    immediate = convert_string_to_ASCII(stringToWrite[index:index+2]);
+                    file.write("    mov word [edi], " + str(immediate) + "\n");
+                    remainingLength -= 2;
+                    index += 2;
+
+                else:
+                    #Write only one character
+                    immediate = convert_string_to_ASCII(stringToWrite[index:index+1]);
+                    file.write("    mov byte [edi], " + str(immediate) + "\n");
+                    remainingLength -= 1;
+                    index += 1;
+        return True;
+    except:
+        return False;
+
+
+
+
 def main():
-    
-    if(write_c_file() == False):
-        print("Failed to write data to .c file: " + str(cFile));
+
+    if(find_perfect_djb2_hash_constant() == False):
+        print("Failed to find perfect hash seed\n");
         return -1;
+        pass;
 
 
-    if(write_h_file() == False):
-        print("Failed to write data to .c file: " + str(hFile));
+    #MUST call this first - since it opens for writing NOT appending
+    if(write_defines() == False):
+        print("Failed to write defines section\n");
         return -2;
+    if(write_data_section() == False):
+        print("Failed to write .data section\n");
+        return -3;
+    if(write_bss_section() == False):
+        print("Failed to write .bss section");
+        return -4;
+    if(write_text_section() == False):
+        print("Failed to write .text section");
+        return -5; 
 
 
+
+    #In .text section write a function that inserts values at their index found in find_perfect_hash_function();
     return 0;
-
-
-
 
 
 
 
 if __name__ == "__main__":
     sys.exit(main());
-
-
-
-
 
 
 
