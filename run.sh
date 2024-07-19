b8d4dc1ffe580cec6e1f5e80f94fa3e0c5cba5b8
KabC03 <kabirc@live.com>
2024-07-19 23:48:11 +1000

    update

---
+++ run.sh
@@ -0,0 +1,38 @@
+#Compile and run everything
+
+
+clear
+
+
+#Run python script
+python3 ./src/set\ compiler\ data.py
+if [[ $? -ne 0 ]]; then
+    
+    echo "\n\nCompiler data was not initialised\n\n"
+    exit -1 
+
+fi
+
+
+#Create directory and compile
+mkdir -p ./output
+clang ./src/*.c ./libraries/*.c -Wall -Werror -fsanitize=address -o ./output/executable
+if [[ $? -ne 0 ]]; then
+
+    echo "\n\nCompilation failed\n\n"
+    exit -2 
+
+fi
+
+
+
+#Run executable
+./output/executable
+if [[ $? -ne 0 ]]; then
+
+    echo "Program did not successfully execute\n\n"
+    exit -3
+
+fi
+
+
